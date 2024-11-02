775f665c8254b132ea25843b5df70a4412ab00bf
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-02 17:07:37 +0800

    Species list and species details are shown and navigation from list to details works.

--- src/species.py
+++ src/species.py
@@ -4,6 +4,11 @@ gi.require_version('Adw', '1')
 from gi.repository import Adw, Gtk
 
 
+@Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/species_list.ui")
+class SpeciesList(Adw.NavigationPage):
+    __gtype_name__ = "SpeciesList"
+
+
 @Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/species_details.ui")
 class SpeciesDetails(Adw.NavigationPage):
     __gtype_name__ = "SpeciesDetails"
