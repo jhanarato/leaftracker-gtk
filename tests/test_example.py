ced6980b235a579bbcce2d0b282fd701cf3113f0
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-07 16:39:47 +0800

    Pytest can run tests on code in source module.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,5 @@
-# from testable import to_call
-import os
+from testable import to_call
 
 
 def test_pytest_installed():
-    assert os.getcwd() == "/app/share/leaftracker-gtk/leaftracker_gtk_tests"
-    # assert to_call()
+    assert to_call()
