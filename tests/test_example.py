c6e2d6e51b9a0b8c240bfbd28360cd27a934fad3
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-07 16:36:44 +0800

    Tests are copied into and can be run from the flatpak.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,7 @@
-import pytest
-
-from testable import to_call
+# from testable import to_call
+import os
 
 
 def test_pytest_installed():
-    assert to_call()
+    assert os.getcwd() == "/app/share/leaftracker-gtk/leaftracker_gtk_tests"
+    # assert to_call()
