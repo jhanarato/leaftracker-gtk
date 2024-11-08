a31bfe7a7521347771e9dd4e0c1aa347000e400d
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-08 08:42:42 +0800

    Remove build scripts as we have a makefile.

--- build.sh
+++
@@ -1,2 +0,0 @@
-flatpak-builder --force-clean --user --install builddir org.bswa.Leaftracker.json
-flatpak run org.bswa.Leaftracker
--- test.sh
+++
@@ -1,2 +0,0 @@
-flatpak-builder --force-clean --user --install builddir org.bswa.Leaftracker.json
-flatpak run --command=pytest --cwd=/app/share/leaftracker-gtk/leaftracker_gtk_tests --devel org.bswa.Leaftracker
