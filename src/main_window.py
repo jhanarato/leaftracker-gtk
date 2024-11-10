2494e311a7c4f5401dc60139aa7f0c6d533e8dda
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-10 10:40:09 +0800

    Revert "Remove import for now."
    This reverts commit 4887058121876f8c5ea06fd4b2d640f84073884e.

--- src/main_window.py
+++ src/main_window.py
@@ -3,6 +3,8 @@ import gi
 gi.require_version('Adw', '1')
 from gi.repository import Adw, Gtk
 
+from .species import SpeciesDetails
+
 
 @Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/main_window.ui")
 class MainWindow(Adw.ApplicationWindow):
