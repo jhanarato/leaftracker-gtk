256869ffacbe556a3bd6f5fb37c94fd532974e92
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-14 10:09:06 +0800

    Pytest can display a window in flatpak.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,8 @@
-from main import main
+from gi.repository import Gio
+resource = Gio.Resource.load('../leaftracker-gtk.gresource')
+resource._register()
+
+from leaftracker_gtk.main import main
 
 
 def test_run_main_function():
