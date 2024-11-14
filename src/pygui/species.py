21851ff7285b5842031cd0c86fb43b4d331684da
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-14 18:36:47 +0800

    SpeciesList can set reference in SpeciesDetail

--- src/pygui/species.py
+++ src/pygui/species.py
@@ -4,11 +4,6 @@ gi.require_version('Adw', '1')
 from gi.repository import Adw, Gtk
 
 
-@Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/species_list.ui")
-class SpeciesList(Adw.NavigationPage):
-    __gtype_name__ = "SpeciesList"
-
-
 @Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/species_details.ui")
 class SpeciesDetails(Adw.NavigationPage):
     __gtype_name__ = "SpeciesDetails"
@@ -20,3 +15,22 @@ class SpeciesDetails(Adw.NavigationPage):
     @property
     def species_reference(self) -> str | None:
         return self._species_reference
+
+    def select_species(self, reference: str) -> None:
+        self._species_reference = reference
+
+
+@Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/species_list.ui")
+class SpeciesList(Adw.NavigationPage):
+    __gtype_name__ = "SpeciesList"
+
+    def __init__(self):
+        super().__init__()
+        self._species_reference = None
+        self._details_page = None
+
+    def set_details_page(self, page: SpeciesDetails) -> None:
+        self._details_page = page
+
+    def select_species(self, reference: str) -> None:
+        self._details_page.select_species(reference)
--- tests/test_species.py
+++ tests/test_species.py
@@ -9,3 +9,12 @@ class TestSpeciesDetails():
     def test_start_in_new_species_mode(self):
         page = SpeciesDetails()
         assert page.species_reference == None
+
+
+class TestSpeciesList():
+    def test_set_species_reference(self):
+        list_page = SpeciesList()
+        details_page = SpeciesDetails()
+        list_page.set_details_page(details_page)
+        list_page.select_species("a_species")
+        assert details_page.species_reference == "a_species"
