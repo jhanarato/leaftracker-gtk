d44cec9ac0c6d48e2e40a14f33e219237468fbdd
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-14 17:27:24 +0800

    Prepare to write species tests.

--- tests/test_species.py
+++ tests/test_species.py
@@ -2,8 +2,8 @@ from gi.repository import Gio
 resource = Gio.Resource.load('../leaftracker-gtk.gresource')
 resource._register()
 
-from pygui.main import main
+from pygui.main_window import MainWindow
 
 
-def test_run_main_function():
-    main(None)
+def test_set_new_species():
+    pass
