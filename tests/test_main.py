b9ee80919d972e17cf80e178e0450391ae68ecc9
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-17 14:58:22 +0800

    Add test to open and close the main window.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,21 @@
+import sys
+
 import gi
 gi.require_version('Adw', '1')
-from gi.repository import Adw
+from gi.repository import Adw, Gio
 
-# Uncommenting this line blows up the tests.
-# from pygui.main_window import MainWindow
+resource = Gio.Resource.load('../leaftracker-gtk.gresource')
+resource._register()
+
+from pygui.main_window import MainWindow
+
+
+def test_open_and_close_main_window():
+    def on_activate(app):
+        window = MainWindow(application=app)
+        window.present()
+        window.close()
+
+    app = Adw.Application(application_id='org.bswa.Leaftracker')
+    app.connect('activate', on_activate)
+    app.run(sys.argv)
