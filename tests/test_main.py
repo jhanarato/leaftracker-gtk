baa59fc45a3fbb2c38de84b1dd39c29aad99f20f
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-17 15:15:21 +0800

    Add test.

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,3 +19,16 @@ def test_open_and_close_main_window():
     app = Adw.Application(application_id='org.bswa.Leaftracker')
     app.connect('activate', on_activate)
     app.run(sys.argv)
+
+
+def test_get_species_details():
+    def on_activate(app):
+        window = MainWindow(application=app)
+        window.present()
+        window.species_details.select_species("xyz")
+        assert window.species_details.species_reference == "xyz"
+        window.close()
+
+    app = Adw.Application(application_id='org.bswa.Leaftracker')
+    app.connect('activate', on_activate)
+    app.run(sys.argv)
