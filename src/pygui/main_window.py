d2a062f77a9bf28164e5773dbbcee56c9ac377e1
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-17 15:45:23 +0800

    MainWindow sets species details page for species list.

--- src/pygui/main_window.py
+++ src/pygui/main_window.py
@@ -3,14 +3,16 @@ import gi
 gi.require_version('Adw', '1')
 from gi.repository import Adw, Gtk
 
-from pygui.species import SpeciesDetails
+from pygui.species import SpeciesDetails, SpeciesList
 
 
 @Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/main_window.ui")
 class MainWindow(Adw.ApplicationWindow):
     __gtype_name__ = "MainWindow"
 
-    species_details = Gtk.Template.Child()
+    species_details: SpeciesDetails = Gtk.Template.Child()
+    species_list: SpeciesList = Gtk.Template.Child()
 
     def __init__(self, application: Adw.Application):
         super().__init__(application=application)
+        self.species_list.set_details_page(self.species_details)
