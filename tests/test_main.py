e0cdc02cd9a401ebe6363d187afed1dd64f1d694
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-17 14:45:53 +0800

    Add test main.

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+import gi
+gi.require_version('Adw', '1')
+from gi.repository import Adw
+
+# from pygui.main_window import MainWindow
+
+
+# def test_get_main_window():
+#     app = Adw.Application(application_id='org.bswa.Leaftracker')
+#     window = MainWindow(app)
+
