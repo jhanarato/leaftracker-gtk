ea38a3994ebcfd5e9cfd7fd8698148b96b3f9ac9
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-21 12:20:27 +0800

    Test property getting & setting.

--- tests/test_properties_and_signals.py
+++ tests/test_properties_and_signals.py
@@ -1,6 +1,8 @@
 from gi.repository import Gio, GObject
 
 
-def test_notification_of_property_change():
+def test_getting_and_setting_properties():
     app = Gio.Application(application_id="foo.bar")
     assert app.get_property("application_id") == "foo.bar"
+    app.set_property("application_id", "foo.bar.baz")
+    assert app.get_property("application_id") == "foo.bar.baz"
