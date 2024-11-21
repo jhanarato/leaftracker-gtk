32eab6eac6d7c6988010056aae91916f7e958474
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-21 12:30:12 +0800

    Create GObject subclass for testing.

--- tests/test_properties_and_signals.py
+++ tests/test_properties_and_signals.py
@@ -1,8 +1,23 @@
 from gi.repository import Gio, GObject
 
 
+class Meaning(GObject.Object):
+    def __init__(self):
+        super().__init__()
+        self._meaning_of_life = 42
+
+    @GObject.Property(type=int)
+    def meaning_of_life(self):
+        return self._meaning_of_life
+
+    @meaning_of_life.setter
+    def meaning_of_life(self, new_meaning):
+        self._meaning_of_life = new_meaning
+
+
 def test_getting_and_setting_properties():
-    app = Gio.Application(application_id="foo.bar")
-    assert app.get_property("application_id") == "foo.bar"
-    app.set_property("application_id", "foo.bar.baz")
-    assert app.get_property("application_id") == "foo.bar.baz"
+    meaning = Meaning()
+    assert meaning.get_property("meaning_of_life") == 42
+    meaning.set_property("meaning_of_life", 43)
+    assert meaning.get_property("meaning_of_life") == 43
+
