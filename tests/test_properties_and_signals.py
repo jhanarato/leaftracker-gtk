173d86b31fdcb4a2bce2301553c3252942aceadb
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-21 12:44:24 +0800

    Add two more tests for property notifications. Both fail and are skipped.

--- tests/test_properties_and_signals.py
+++ tests/test_properties_and_signals.py
@@ -1,3 +1,4 @@
+import pytest
 from gi.repository import Gio, GObject
 
 
@@ -21,3 +22,32 @@ def test_getting_and_setting_properties():
     meaning.set_property("meaning_of_life", 43)
     assert meaning.get_property("meaning_of_life") == 43
 
+
+@pytest.mark.skip("Try with app first")
+def test_changing_property_invokes_notifier():
+    meaning_changed = False
+
+    def on_notify():
+        nonlocal meaning_changed
+        meaning_changed = True
+
+    meaning = Meaning()
+    meaning.connect("notify::meaning_of_life", on_notify)
+    meaning.meaning_of_life = 43
+
+    assert meaning_changed
+
+
+@pytest.mark.skip("Also fails")
+def test_changing_app_property_invokes_notifier():
+    app_id_changed = False
+
+    def on_notify():
+        nonlocal app_id_changed
+        app_id_changed = True
+
+    app = Gio.Application(application_id="foo.bar")
+    app.connect("notify::application-id", on_notify)
+    app.set_application_id("foo.baz")
+
+    assert app_id_changed
