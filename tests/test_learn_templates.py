9db56d93d0758eade8f21472efafc8677bb22910
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-29 13:23:21 +0800

    Delete test. Delete swapped attribute.

--- tests/test_learn_templates.py
+++ tests/test_learn_templates.py
@@ -3,31 +3,11 @@ import gi
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk, GObject
 
-template_defining_property = """\
-<interface>
-    <template class="WidgetWithTemplateDefinedProperty" parent="GtkBox">
-        <property name="template-defined-property">1</property>
-    </template>
-</interface>
-"""
-
-
-@Gtk.Template(string=template_defining_property)
-class WidgetWithTemplateDefinedProperty(Gtk.Box):
-    # This needs to match the template, the class name doesn't
-    __gtype_name__ = "WidgetWithTemplateDefinedProperty"
-
-
-def test_cannot_define_property_in_template():
-    widget = WidgetWithTemplateDefinedProperty()
-    with pytest.raises(TypeError, match="does not have property `template-defined-property'"):
-        widget.set_property("template-defined-property", 1)
-
 
 callback_template = """\
 <interface>
     <template class="CallbackWidget" parent="GtkBox">
-        <signal name="notify::custom-parameter" handler="parameter_changed"  swapped="no" />
+        <signal name="notify::custom-parameter" handler="parameter_changed"/>
     </template>
 </interface>
 """
