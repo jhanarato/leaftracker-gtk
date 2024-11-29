fca3c4d9e5c7becb2deaf0524f9d0e1377357a80
Ajahn Jhanarato <jhanarato@gmail.com>
2024-11-29 13:18:40 +0800

    Add learning test: test_changing_property_calls_callback

--- tests/test_learn_templates.py
+++ tests/test_learn_templates.py
@@ -1,7 +1,7 @@
 import pytest
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
 
 template_defining_property = """\
 <interface>
@@ -24,9 +24,42 @@ def test_cannot_define_property_in_template():
         widget.set_property("template-defined-property", 1)
 
 
+callback_template = """\
+<interface>
+    <template class="CallbackWidget" parent="GtkBox">
+        <signal name="notify::custom-parameter" handler="parameter_changed"  swapped="no" />
+    </template>
+</interface>
+"""
 
-@pytest.mark.skip("Work on other test first.")
+
+@Gtk.Template(string=callback_template)
+class CallbackWidget(Gtk.Box):
+    __gtype_name__ = "CallbackWidget"
+
+    def __init__(self):
+        super().__init__()
+        self._custom_parameter_value = 0
+        self._callback_called = False
+
+    @GObject.Property(type=int)
+    def custom_parameter(self) -> int:
+        return self._custom_parameter_value
+
+    @custom_parameter.setter
+    def custom_parameter(self, new_value: int) -> None:
+        self._custom_parameter_value = new_value
+
+    @property
+    def callback_called(self) -> bool:
+        return self._callback_called
+
+    @Gtk.Template.Callback()
+    def parameter_changed(self, *args):
+        self._callback_called = True
+
+
 def test_changing_property_calls_callback():
-    button = CallbackButton()
-    button.string_parameter = "Frosty Fruits"
-    assert button.callback_called
+    button = CallbackWidget()
+    button.custom_parameter = 1
+    assert button.callback_called is True
