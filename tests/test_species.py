d4017df77b62b39d1935256dbb9066bff3637636
jr <jhanarato@gmail.com>
2024-12-21 12:53:32 +0800

    Extract fixture.

--- tests/test_species.py
+++ tests/test_species.py
@@ -1,3 +1,4 @@
+import pytest
 from gi.repository import Gio, GObject
 
 resource = Gio.Resource.load('../leaftracker-gtk.gresource')
@@ -5,30 +6,29 @@ resource._register()
 
 from pygui.species import SpeciesDetailsPage
 
+@pytest.fixture
+def details_page():
+    return SpeciesDetailsPage()
+
 
 class TestSpeciesDetailsPage:
-    def test_set_species_reference(self):
-        details_page = SpeciesDetailsPage()
+    def test_set_species_reference(self, details_page):
         details_page.set_property("reference", "abc")
         assert details_page.get_property("reference") == "abc"
 
-    def test_set_species_reference_to_none(self):
-        details_page = SpeciesDetailsPage()
+    def test_set_species_reference_to_none(self, details_page):
         details_page.set_property("reference", None)
         assert details_page.get_property("reference") is None
 
-    def test_reference_display_is_updated_when_reference_changed(self):
-        details_page = SpeciesDetailsPage()
+    def test_reference_display_is_updated_when_reference_changed(self, details_page):
         details_page.set_property("reference", "xyz")
         assert details_page.reference_display.get_text() == "xyz"
 
-    def test_setting_reference_to_none_shows_missing_message(self):
-        details_page = SpeciesDetailsPage()
+    def test_setting_reference_to_none_shows_missing_message(self, details_page):
         details_page.set_property("reference", None)
         assert details_page.reference_display.get_text() == "None"
 
-    def test_changing_bound_property_sets_reference(self, gobject_with_property):
-        details_page = SpeciesDetailsPage()
+    def test_changing_bound_property_sets_reference(self, details_page, gobject_with_property):
         gobject_with_property.bind_property(
             source_property="prop-a",
             target=details_page,
@@ -37,3 +37,4 @@ class TestSpeciesDetailsPage:
         )
         gobject_with_property.set_property("prop-a", "cba")
         assert details_page.reference == "cba"
+
