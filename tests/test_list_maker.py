b6117175783dc829b713906143659ff26d5ebaa6
jr <jhanarato@gmail.com>
2025-01-16 17:17:21 +0800

    Refactor: move tests for PreviousScientificNames to new module.

--- tests/test_list_maker.py
+++ tests/test_list_maker.py
@@ -1,10 +1,12 @@
 import pytest
 from gi.repository import Gio, Gtk, GObject
 
+
 resource = Gio.Resource.load('../leaftracker-gtk.gresource')
 resource._register()
 
 from pygui.list_maker import RemovableRow
+from pygui.species import PreviousScientificNames
 
 class TestRemovableRow:
     def test_emits_remove_signal(self):
@@ -36,3 +38,34 @@ class TestRemovableRow:
     def test_get_text(self):
         widget = RemovableRow("Row text")
         assert widget.get_text() == "Row text"
+
+
+class TestPreviousScientificNames:
+    def test_can_add_name(self):
+        widget = PreviousScientificNames()
+        widget.name_field = "Acacia saligna"
+        widget.click_add_button()
+        name = widget.get_name_from_item(0)
+        assert name == "Acacia saligna"
+
+    def test_name_field_cleared_after_add(self):
+        widget = PreviousScientificNames()
+        widget.name_field = "Acacia saligna"
+        widget.click_add_button()
+        assert widget.name_field == ""
+
+    def test_can_get_list_of_names(self):
+        widget = PreviousScientificNames()
+        widget.name_field = "Acacia abc"
+        widget.click_add_button()
+        widget.name_field = "Acacia xyz"
+        widget.click_add_button()
+        assert widget.get_species_names() == ["Acacia abc", "Acacia xyz"]
+
+    def test_adding_a_duplicate_name_does_not_modify_the_list(self):
+        widget = PreviousScientificNames()
+        widget.name_field = "Acacia saligna"
+        widget.click_add_button()
+        widget.name_field = "Acacia saligna"
+        widget.click_add_button()
+        assert widget.get_species_names() == ["Acacia saligna"]
--- tests/test_species.py
+++ tests/test_species.py
@@ -32,37 +32,6 @@ class TestFakeSpeciesWriter:
         writer = FakeSpeciesWriter()
 
 
-class TestPreviousScientificNames:
-    def test_can_add_name(self):
-        widget = PreviousScientificNames()
-        widget.name_field = "Acacia saligna"
-        widget.click_add_button()
-        name = widget.get_name_from_item(0)
-        assert name == "Acacia saligna"
-
-    def test_name_field_cleared_after_add(self):
-        widget = PreviousScientificNames()
-        widget.name_field = "Acacia saligna"
-        widget.click_add_button()
-        assert widget.name_field == ""
-
-    def test_can_get_list_of_names(self):
-        widget = PreviousScientificNames()
-        widget.name_field = "Acacia abc"
-        widget.click_add_button()
-        widget.name_field = "Acacia xyz"
-        widget.click_add_button()
-        assert widget.get_species_names() == ["Acacia abc", "Acacia xyz"]
-
-    def test_adding_a_duplicate_name_does_not_modify_the_list(self):
-        widget = PreviousScientificNames()
-        widget.name_field = "Acacia saligna"
-        widget.click_add_button()
-        widget.name_field = "Acacia saligna"
-        widget.click_add_button()
-        assert widget.get_species_names() == ["Acacia saligna"]
-
-
 class TestSpeciesDetailsPage:
     def test_set_species_reference(self, details_page):
         details_page.set_property("reference", "abc")
