63d17ae4bdb2d62a6c91212713fb821dd1433e2a
jr <jhanarato@gmail.com>
2025-01-16 13:23:23 +0800

    Add learning test: How to remove a matching string in a StringList

--- tests/test_learn_models.py
+++ tests/test_learn_models.py
@@ -12,7 +12,7 @@ def test_length(string_list):
     assert len(string_list) == 3
 
 
-def get_position_of_matching_string(string_list: list[str], to_match: str) -> int | None:
+def get_position_of_matching_string(string_list: Gtk.StringList, to_match: str) -> int | None:
     for position, item in enumerate(string_list):
         if item.get_string() == to_match:
             return position
@@ -24,3 +24,16 @@ def get_position_of_matching_string(string_list: list[str], to_match: str) -> in
 )
 def test_get_position_of_matching_string(string_list, value, position):
     assert get_position_of_matching_string(string_list, value) == position
+
+
+def remove_matching_string(string_list: Gtk.StringList, to_match: str) -> None:
+    position = get_position_of_matching_string(string_list, to_match)
+    string_list.remove(position)
+
+
+def test_remove_matching_string(string_list):
+    remove_matching_string(string_list, "abc")
+    assert len(string_list) == 2
+    assert string_list.get_item(0).get_string() == "def"
+    assert string_list.get_item(1).get_string() == "hij"
+
