ee4678ba806e0ea2c65d55196fb3e9298ca16568
jr <jhanarato@gmail.com>
2025-01-16 13:17:17 +0800

    New learning test lets us get the position of a matching string.

--- tests/test_learn_models.py
+++ tests/test_learn_models.py
@@ -7,5 +7,20 @@ import pytest
 def string_list() -> Gtk.StringList:
     return Gtk.StringList.new(["abc", "def", "hij"])
 
+
 def test_length(string_list):
     assert len(string_list) == 3
+
+
+def get_position_of_matching_string(string_list: list[str], to_match: str) -> int | None:
+    for position, item in enumerate(string_list):
+        if item.get_string() == to_match:
+            return position
+    return None
+
+@pytest.mark.parametrize(
+    "value,position",
+    [("abc", 0), ("def", 1), ("hij", 2)]
+)
+def test_get_position_of_matching_string(string_list, value, position):
+    assert get_position_of_matching_string(string_list, value) == position
