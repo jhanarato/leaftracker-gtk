e7340f8a001ff5ed49f05e41883d90ce84d2381b
jr <jhanarato@gmail.com>
2025-01-16 13:04:52 +0800

    Add simple fixture and test for Gtk.StringList

--- tests/test_learn_models.py
+++ tests/test_learn_models.py
@@ -1,2 +1,11 @@
-def test_will_pass():
-    assert True
+import gi
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk
+import pytest
+
+@pytest.fixture
+def string_list() -> Gtk.StringList:
+    return Gtk.StringList.new(["abc", "def", "hij"])
+
+def test_length(string_list):
+    assert len(string_list) == 3
