01292e38caa480cc7fa6973f6b2b4c5ea4a51355
jr <jhanarato@gmail.com>
2025-01-19 10:09:25 +0800

    Tidy: rename UI file to match class

--- src/pygui/species.py
+++ src/pygui/species.py
@@ -6,7 +6,7 @@ gi.require_version('Adw', '1')
 from gi.repository import Adw, Gtk
 
 
-@Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/species_list.ui")
+@Gtk.Template(resource_path="/org/bswa/Leaftracker/ui/species_list_page.ui")
 class SpeciesListPage(Adw.NavigationPage):
     __gtype_name__ = "SpeciesListPage"
 
