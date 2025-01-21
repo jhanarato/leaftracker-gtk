a6cedba0de2f5057101493f09ce0672ae1a7b6e7
jr <jhanarato@gmail.com>
2025-01-21 17:41:11 +0800

    Extract fixtures always_valid and always_invalid

--- tests/test_validated_entry_row.py
+++ tests/test_validated_entry_row.py
@@ -7,6 +7,17 @@ resource._register()
 
 from pygui.validated_entry_row import ValidatedEntryRow
 
+@pytest.fixture
+def always_valid() -> ValidatedEntryRow:
+    widget = ValidatedEntryRow()
+    widget.set_validator(lambda value: True)
+    return widget
+
+@pytest.fixture
+def always_invalid() -> ValidatedEntryRow:
+    widget = ValidatedEntryRow()
+    widget.set_validator(lambda value: False)
+    return widget
 
 class TestValidatedEntryRow:
     def test_entry_is_valid_by_default(self):
@@ -25,22 +36,16 @@ class TestValidatedEntryRow:
         widget.set_text("some text")
         assert widget.get_show_apply_button()
 
-    def test_valid_text_shows_apply_button(self):
-        widget = ValidatedEntryRow()
-        widget.set_validator(lambda value: True)
-        widget.set_text("some text")
-        assert widget.get_show_apply_button()
+    def test_valid_text_shows_apply_button(self, always_valid):
+        always_valid.set_text("some text")
+        assert always_valid.get_show_apply_button()
 
-    def test_invalid_text_hides_apply_button(self):
-        widget = ValidatedEntryRow()
-        widget.set_validator(lambda value: False)
-        widget.set_text("some text")
-        assert not widget.get_show_apply_button()
+    def test_invalid_text_hides_apply_button(self, always_invalid):
+        always_invalid.set_text("some text")
+        assert not always_invalid.get_show_apply_button()
 
-    def test_reset_after_valid_input(self):
-        widget = ValidatedEntryRow()
-        widget.set_validator(lambda value: True)
-        widget.set_text("some text")
-        widget.reset()
-        assert widget.get_text() == ""
-        assert widget.get_show_apply_button()
+    def test_reset_after_valid_input(self, always_valid):
+        always_valid.set_text("some text")
+        always_valid.reset()
+        assert always_valid.get_text() == ""
+        assert always_valid.get_show_apply_button()
