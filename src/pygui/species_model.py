0bb6bb5cbd26247623c58f7d0d89c8dea60fdf8c
jr <jhanarato@gmail.com>
2025-01-23 16:57:07 +0800

    Add current_name property to SpeciesModel.

--- src/pygui/species_model.py
+++ src/pygui/species_model.py
@@ -6,6 +6,7 @@ class SpeciesModel(GObject.Object):
     def __init__(self):
         super().__init__()
         self._reference: str | None = None
+        self._current_name: str | None = None
 
     @GObject.Property(type=str)
     def reference(self) -> str | None:
@@ -14,3 +15,11 @@ class SpeciesModel(GObject.Object):
     @reference.setter
     def reference(self, value: str):
         self._reference = value
+
+    @GObject.Property(type=str)
+    def current_name(self) -> str | None:
+        return self._current_name
+
+    @current_name.setter
+    def current_name(self, value: str) -> str | None:
+        self._current_name = value
--- tests/test_species_model.py
+++ tests/test_species_model.py
@@ -2,11 +2,14 @@ from pygui.species_model import SpeciesModel
 
 
 class TestSpeciesModel:
-    def test_when_created_reference_is_none(self):
+    def test_reference(self):
         species = SpeciesModel()
         assert species.reference is None
-
-    def test_can_set_reference(self):
-        species = SpeciesModel()
         species.reference = "reference"
         assert species.reference == "reference"
+
+    def test_current_name(self):
+        species = SpeciesModel()
+        assert species.current_name is None
+        species.current_name = "current name"
+        assert species.current_name == "current name"
