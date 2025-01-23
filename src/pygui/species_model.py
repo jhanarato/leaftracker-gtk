5977e60ff742c23de4f9e92ffdab2566b6d05794
jr <jhanarato@gmail.com>
2025-01-23 16:40:00 +0800

    SpeciesModel has a reference property.

--- src/pygui/species_model.py
+++ src/pygui/species_model.py
@@ -5,3 +5,8 @@ from gi.repository import Gio, GObject
 class SpeciesModel(GObject.Object):
     def __init__(self):
         super().__init__()
+        self._reference: str | None = None
+
+    @GObject.Property(type=str)
+    def reference(self) -> str | None:
+        return self._reference
--- tests/test_species_model.py
+++ tests/test_species_model.py
@@ -2,5 +2,6 @@ from pygui.species_model import SpeciesModel
 
 
 class TestSpeciesModel:
-    def create_species(self):
+    def test_when_created_reference_is_none(self):
         species = SpeciesModel()
+        assert species.reference is None
