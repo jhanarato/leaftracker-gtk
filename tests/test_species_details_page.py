bd85ff24f53493021e1c4d2123e8db767b1ae946
jr <jhanarato@gmail.com>
2025-01-30 17:06:14 +0800

    Tidy: rename and reorder tests.

--- tests/test_species_details_page.py
+++ tests/test_species_details_page.py
@@ -9,17 +9,29 @@ from pygui.species_details_page import SpeciesDetailsPage
 
 
 class TestSpeciesDetailsPage:
-    def test_can_set_species_reference_to_none(self):
+    def test_species_property(self):
+        details_page = SpeciesDetailsPage()
+        details_page.set_property("current_species", SpeciesModel())
+        assert details_page.get_property("current_species") == SpeciesModel()
+
+    def test_can_set_species_property_to_none(self):
         details_page = SpeciesDetailsPage()
         details_page.set_property("current_species", None)
         assert details_page.get_property("current_species") is None
 
-    def test_setting_reference_to_none_shows_missing_message(self):
+    def test_when_reference_is_none_it_is_displayed(self):
         details_page = SpeciesDetailsPage()
         species = SpeciesModel()
         species.reference = None
         details_page.set_property("current_species", species)
         assert details_page.reference_display.get_text() == "None"
+
+    def test_reference_display_updated_when_current_species_changed(self):
+        page = SpeciesDetailsPage()
+        species = SpeciesModel()
+        species.reference = "reference-xyz"
+        page.current_species = species
+        assert page.reference_display.get_text() == "reference-xyz"
 
     @pytest.mark.skip("Keep obsolete test as documentation.")
     def test_changing_bound_property_sets_reference(self, gobject_with_property):
@@ -32,15 +44,3 @@ class TestSpeciesDetailsPage:
         )
         gobject_with_property.set_property("prop-a", "cba")
         assert details_page.reference == "cba"
-
-    def test_species_property(self):
-        details_page = SpeciesDetailsPage()
-        details_page.set_property("current_species", SpeciesModel())
-        assert details_page.get_property("current_species") == SpeciesModel()
-
-    def test_reference_display_updated_when_current_species_changed(self):
-        page = SpeciesDetailsPage()
-        species = SpeciesModel()
-        species.reference = "reference-xyz"
-        page.current_species = species
-        assert page.reference_display.get_text() == "reference-xyz"
