c020796a4346a7f80d51359a605a4033e4781114
jr <jhanarato@gmail.com>
2025-01-30 17:27:50 +0800

    Replace old test for binding properties with new one using SpeciesDetailsPage and SpeciesListPage

--- tests/test_species_details_page.py
+++ tests/test_species_details_page.py
@@ -1,11 +1,13 @@
 import pytest
 from gi.repository import Gio, Gtk, GObject
 
+
 resource = Gio.Resource.load('../leaftracker-gtk.gresource')
 resource._register()
 
 from pygui.species_model import SpeciesModel
 from pygui.species_details_page import SpeciesDetailsPage
+from pygui.species_list_page import SpeciesListPage
 
 
 class TestSpeciesDetailsPage:
@@ -33,14 +35,17 @@ class TestSpeciesDetailsPage:
         page.current_species = species
         assert page.reference_display.get_text() == "reference-xyz"
 
-    @pytest.mark.skip("Keep obsolete test as documentation.")
-    def test_changing_bound_property_sets_reference(self, gobject_with_property):
+    def test_can_bind_current_and_selected_species(self):
         details_page = SpeciesDetailsPage()
-        gobject_with_property.bind_property(
-            source_property="prop-a",
-            target=details_page,
-            target_property="reference",
+        list_page = SpeciesListPage()
+        details_page.bind_property(
+            source_property="current_species",
+            target=list_page,
+            target_property="selected_species",
             flags=GObject.BindingFlags.BIDIRECTIONAL
         )
-        gobject_with_property.set_property("prop-a", "cba")
-        assert details_page.reference == "cba"
+
+        species = SpeciesModel()
+        species.reference = "reference-xyz"
+        list_page.selected_species = species
+        assert details_page.current_species == species
