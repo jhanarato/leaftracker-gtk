fbb5001bbc57a8c081561e3e11b4f361d920e90d
jr <jhanarato@gmail.com>
2025-02-05 17:56:37 +0800

    Add learning test: the notify signal.

--- tests/test_learn_signals.py
+++ tests/test_learn_signals.py
@@ -15,6 +15,14 @@ class Signaller(GObject.Object):
     def signal_with_side_effect(self) -> None:
         print("A side effect of emit")
 
+    @GObject.Property(type=str)
+    def static_property(self) -> str:
+        return "static property"
+
+    @static_property.setter
+    def static_property(self, value: str):
+        pass
+
 
 class TestSignals:
     def test_noarg_signal(self):
@@ -26,3 +34,18 @@ class TestSignals:
         signaller.emit("signal-with-side-effect")
         captured = capsys.readouterr()
         assert captured.out == "A side effect of emit\n"
+
+    def test_handle_notify_signal(self):
+        param_passed = None
+
+        def callback(instance, param):
+            print("In callback")
+            nonlocal param_passed
+            param_passed = param
+
+        signaller = Signaller()
+        # Note: static-property is referred to as a "detail"
+        signaller.connect("notify::static-property", callback)
+        signaller.static_property = "This changes nothing"
+
+        assert isinstance(param_passed, GObject.ParamSpecString)
