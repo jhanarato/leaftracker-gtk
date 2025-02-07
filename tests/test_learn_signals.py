c41f60cb1abd15d22b2110fb7ac48964a87779fc
jr <jhanarato@gmail.com>
2025-02-07 10:36:21 +0800

    Reorganise test module.

--- tests/test_learn_signals.py
+++ tests/test_learn_signals.py
@@ -27,42 +27,80 @@ class Signaller(GObject.Object):
     def static_property(self, value: str):
         pass
 
+class NoargSignaller(GObject.Object):
+    def __init__(self):
+        super().__init__()
 
-class TestSignals:
-    def test_noarg_signal(self):
-        signaller = Signaller()
-        signaller.emit("noarg-signal")
+    @GObject.Signal
+    def noarg_signal(self) -> None:
+        pass
 
-    def test_signal_with_side_effect(self, capsys):
-        signaller = Signaller()
-        signaller.emit("signal-with-side-effect")
-        captured = capsys.readouterr()
-        assert captured.out == "A side effect of emit\n"
+def test_noarg_signal():
+    signaller = NoargSignaller()
+    signaller.emit("noarg-signal")
 
-    def test_signal_with_return_type(self):
-        instance_type = None
-        def callback(instance: Signaller) -> bool:
-            nonlocal instance_type
-            instance_type = type(instance)
-            return True
 
-        signaller = Signaller()
-        signaller.connect("signal-with-return-type", callback)
-        signaller.emit("signal-with-return-type")
+class SideEffectSignaller(GObject.Object):
+    def __init__(self):
+        super().__init__()
 
-        assert instance_type == Signaller
+    @GObject.Signal
+    def signal_with_side_effect(self) -> None:
+        print("A side effect of emit")
 
-    def test_handle_notify_signal(self):
-        param_passed = None
+def test_signal_with_side_effect(capsys):
+    signaller = SideEffectSignaller()
+    signaller.emit("signal-with-side-effect")
+    captured = capsys.readouterr()
+    assert captured.out == "A side effect of emit\n"
 
-        def callback(instance, param):
-            nonlocal param_passed
-            param_passed = param
 
-        signaller = Signaller()
-        # Note: static-property is referred to as a "detail"
-        signaller.connect("notify::static-property", callback)
-        signaller.static_property = "This changes nothing"
+class ReturnTypeSignaller(GObject.Object):
+    def __init__(self):
+        super().__init__()
 
-        assert isinstance(param_passed, GObject.ParamSpecString)
+    @GObject.Signal(return_type=bool)
+    def signal_with_return_type(self) -> bool:
+        pass
+
+def test_signal_with_return_type():
+    instance_type = None
+    def callback(instance: Signaller) -> bool:
+        nonlocal instance_type
+        instance_type = type(instance)
+        return True
+
+    signaller = Signaller()
+    signaller.connect("signal-with-return-type", callback)
+    signaller.emit("signal-with-return-type")
+
+    assert instance_type == Signaller
+
+
+class NotifySignaller(GObject.Object):
+    def __init__(self):
+        super().__init__()
+
+    @GObject.Property(type=str)
+    def static_property(self) -> str:
+        return "static property"
+
+    @static_property.setter
+    def static_property(self, value: str):
+        pass
+
+
+def test_handle_notify_signal():
+    param_passed = None
+
+    def callback(instance, param):
+        nonlocal param_passed
+        param_passed = param
+
+    signaller = NotifySignaller()
+    # Note: static-property is referred to as a "detail"
+    signaller.connect("notify::static-property", callback)
+    signaller.static_property = "This changes nothing"
+
+    assert isinstance(param_passed, GObject.ParamSpecString)
 
