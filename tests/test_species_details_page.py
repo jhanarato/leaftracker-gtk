3e14f688a84398ba903f7d81332624ee6e4bff82
jr <jhanarato@gmail.com>
2025-02-07 17:12:51 +0800

    Extract fixture.

--- tests/test_species_details_page.py
+++ tests/test_species_details_page.py
@@ -8,6 +8,13 @@ resource._register()
 from pygui.species_model import SpeciesModel
 from pygui.species_details_page import SpeciesDetailsPage
 
+@pytest.fixture
+def species_data() -> SpeciesModel:
+    species = SpeciesModel()
+    species.reference = "reference-xyz"
+    species.current_name = "Acacia saligna"
+    species.previous_names = ["Acacia old", "Acacia older"]
+    return species
 
 class TestSpeciesDetailsPage:
     def test_species_property(self):
@@ -27,16 +34,12 @@ class TestSpeciesDetailsPage:
         details_page.set_property("current_species", species)
         assert details_page.reference_display.get_text() == "None"
 
-    def test_when_current_species_set_fields_are_populated(self):
+    def test_when_current_species_set_fields_are_populated(self, species_data):
         page = SpeciesDetailsPage()
-        species = SpeciesModel()
-        species.reference = "reference-xyz"
-        species.current_name = "Acacia saligna"
-        species.previous_names = ["Acacia old", "Acacia older"]
-        page.current_species = species
-        assert page.reference_display.get_text() == "reference-xyz"
-        assert page.current_scientific_name.get_text() == "Acacia saligna"
-        assert page.previous_scientific_names.get_values() == ["Acacia old", "Acacia older"]
+        page.current_species = species_data
+        assert page.reference_display.get_text() == species_data.reference
+        assert page.current_scientific_name.get_text() == species_data.current_name
+        assert page.previous_scientific_names.get_values() == species_data.previous_names
 
     def test_when_current_name_is_changed_the_edited_species_is_updated(self):
         page = SpeciesDetailsPage()
