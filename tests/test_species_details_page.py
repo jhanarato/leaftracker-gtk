976be82c2deafe115637f7e262575a085897930d
jr <jhanarato@gmail.com>
2025-02-10 12:49:13 +0800

    Remove skip on passing tests.

--- tests/test_species_details_page.py
+++ tests/test_species_details_page.py
@@ -37,13 +37,11 @@ class TestSpeciesDetailsPage:
         page.current_species = species_data
         assert page.current_species == species_data.clone()
 
-    @pytest.mark.skip("Get back to this")
     def test_when_current_species_set_to_none_edited_species_is_new_species(self):
         page = SpeciesDetailsPage()
         page.current_species = None
         assert page.edited_species == SpeciesModel()
 
-    @pytest.mark.skip("Get back to this")
     def test_when_new_current_species_set_to_new_species_edited_is_new_species(self):
         page = SpeciesDetailsPage()
         page.current_species = SpeciesModel()
