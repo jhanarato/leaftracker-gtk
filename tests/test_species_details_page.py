b7d0e59a30bfb5fbcc0b8986604da670ccf3da7f
jr <jhanarato@gmail.com>
2025-02-20 13:04:11 +0800

    Use properties to access protected attributes.

--- tests/test_species_details_page.py
+++ tests/test_species_details_page.py
@@ -74,24 +74,24 @@ class TestSpeciesDetailsPage:
     def test_when_current_name_is_changed_the_edited_species_is_updated(self):
         page = SpeciesDetailsPage()
         page.current_scientific_name.set_text("Eucalyptus rudis")
-        assert page._edited_species.current_name == "Eucalyptus rudis"
+        assert page.edited_species.current_name == "Eucalyptus rudis"
 
     def test_when_previous_names_are_changed_the_edited_species_is_updated(self):
         page = SpeciesDetailsPage()
         page.previous_scientific_names.add_string("Acacia old")
         page.previous_scientific_names.add_string("Acacia older")
-        assert page._edited_species.previous_names == ["Acacia old", "Acacia older"]
+        assert page.edited_species.previous_names == ["Acacia old", "Acacia older"]
 
     def test_when_current_species_is_set_edited_is_cloned(self, species_data):
         page = SpeciesDetailsPage()
 
-        assert id(page._current_species) != id(page._edited_species)
-        assert page._current_species == page._edited_species
+        assert id(page.current_species) != id(page.edited_species)
+        assert page.current_species == page.edited_species
 
         page.current_species = species_data
 
-        assert id(page._current_species) != id(page._edited_species)
-        assert page._current_species == page._edited_species
+        assert id(page.current_species) != id(page.edited_species)
+        assert page.current_species == page.edited_species
 
     def test_save_is_not_sensitive_by_default(self):
         page = SpeciesDetailsPage()
